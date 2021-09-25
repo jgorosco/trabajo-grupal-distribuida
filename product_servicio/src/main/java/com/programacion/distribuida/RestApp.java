79c533c62427eeff73248ee8e4add8f5f545a71f
jonato96 <80604082+jonato96@users.noreply.github.com>
2021-09-25 10:19:27 -0500

    microservicio producto

---
+++ product_servicio/src/main/java/com/programacion/distribuida/RestApp.java
@@ -0,0 +1,9 @@
+package com.programacion.distribuida;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApp extends Application {
+
+}
